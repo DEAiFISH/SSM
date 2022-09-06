1dc68b83752cc8fbdb6dcf413fdaac1ce0f86af6
DEAIFISH <1277676309@qq.com>
2022-09-06 16:05:19 +0800

    2022/09/06

---
+++ spring-aop/src/main/java/Annotation/LoggerAspect.java
@@ -0,0 +1,11 @@
+package Annotation;
+
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect//将当前组件标识为切面
+
+public class LoggerAspect {
+}

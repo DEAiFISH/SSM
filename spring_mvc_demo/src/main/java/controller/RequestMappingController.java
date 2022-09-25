1b3519c7bc705ac859687ecafa5e90398caa4542
DEAIFISH <1277676309@qq.com>
2022-09-25 22:42:36 +0800

    2022/09/25

--- spring_mvc_demo/src/main/java/controller/RequestMappingController.java
+++ spring_mvc_demo/src/main/java/controller/RequestMappingController.java
@@ -1,8 +1,14 @@
 package controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping({"/test"})
 public class RequestMappingController {
 
+    @RequestMapping({"/hello","/234"})
+    public String hello(){
+        return "success";
+    }
 }

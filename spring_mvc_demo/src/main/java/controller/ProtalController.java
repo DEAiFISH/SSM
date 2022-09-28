e29fd2f25503f40d1c965dda680f1467237813f3
DEAIFISH <1277676309@qq.com>
2022-09-28 17:21:25 +0800

    2022/09/28

--- spring_mvc_demo/src/main/java/controller/ProtalController.java
+++ spring_mvc_demo/src/main/java/controller/ProtalController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class ProtalController {
 
-    @RequestMapping("/")
+    /*@RequestMapping("/")
     public String protal() {
         return "index";
-    }
+    }*/
 }

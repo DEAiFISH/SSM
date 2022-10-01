897a52faef7de31b39f937899b5f08d5c7d80a32
DEAIFISH <1277676309@qq.com>
2022-10-02 02:09:42 +0800

    2022/10/02

---
+++ spring_mvc_annotation/src/main/java/com/DEAiFISH/config/SpringConfig.java
@@ -0,0 +1,15 @@
+package com.DEAiFISH.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 代替spring配置文件
+ *
+ */
+@Configuration
+public class SpringConfig {
+
+
+
+
+}
---
+++ spring_mvc_annotation/src/main/java/com/DEAiFISH/controller/TestController.java
@@ -0,0 +1,7 @@
+package com.DEAiFISH.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class TestController {
+}
